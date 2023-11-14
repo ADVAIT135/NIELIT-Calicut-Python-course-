2143b978a69944a82c98b109bbc113bff88163dd
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2023-11-14 20:47:41 +0530

    Exercise - 9
    Python code  + Output

---
+++ Exercise - 9/Exercise 9.py
@@ -0,0 +1,20 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Excercise No 9
+"""Task -> Write a program that asks for your age in human years. 
+Hint: Store the age in a variable and calculate your age in bird years. 
+1 human year equals 9 bird years. 
+ 
+If I am 1 year old in human years, my age in bird years is 9.If 
+I am 2, then my age in bird years would be 18 and so on. 
+ 
+The formula then is this: 
+Age in bird years = Age in human years * 9 """
+
+
+print("Hi There!!!")
+print("Welcome to Age in Bird Years program")
+name = input("Enter your name : ")
+age_human = int(input(f"Hi {name}, enter your age here : "))
+age_birds = age_human * 9
+print(f"So {name} your age in human years is {age_human} and your age in bird years is {age_birds}")
