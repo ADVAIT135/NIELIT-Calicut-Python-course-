c5fd3b8b59786ba9f9cda05d18ca5cc6f81722de
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2023-11-14 20:14:18 +0530

    Exercise 7
    Python file + doc file

---
+++ Exercise - 7/Exercise 7A.py
@@ -0,0 +1,12 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Excercise No 7 - A
+print(3+2*-1)
+print((3+2)*4-1)
+print(14/3)
+print(14//3)
+print(14%3)
+print(4**2)
+print(2**3**4)
+print(245.34+945.55)
+print(10498237/4323)
---
+++ Exercise - 7/Exercise 7B.py
@@ -0,0 +1,9 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Excercise No 7 - B
+a = 1000
+b = 50
+c = a / b + 2
+d = c * 10 % 5
+d = d + 1
+print(d)
