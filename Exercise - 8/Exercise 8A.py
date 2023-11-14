6b28ed28dc964855f52a75b24a15e09da6e8fb9c
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2023-11-14 20:28:59 +0530

    Exercise 8
    Python codes + doc file

---
+++ Exercise - 8/Exercise 8A.py
@@ -0,0 +1,12 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Excercise No 8 - A
+print(9+4*2-1)
+print((3+7)*4-1)
+print(14 / 3 )
+print(14 // 3 )
+print(14%3)
+print(4**2)
+print(2**3**4)
+print(245.347 + 945.775)
+print(10498237 / 4323)
---
+++ Exercise - 8/Exercise 8B.py
@@ -0,0 +1,9 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Excercise No 8 - B
+a = 1000
+b = 50
+c = a / b + 2
+pl = c * 10 
+pl = pl + 1
+print(pl)
