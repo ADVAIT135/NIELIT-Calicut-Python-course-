ab3856f8764d76a765fffed15893c80c8a545cde
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2023-11-14 21:47:52 +0530

    Exercise 16
    Task Pdf + Python file + Output Pics

---
+++ Exercise - 16/exercise16.py
@@ -0,0 +1,15 @@
+## NIELIT Calicut python Training session
+## ADVAIT GURUNATH CHAVAN
+## Exercise No 16
+"""Using loops create a program named exercise16.py 
+Count from 50 to 100 
+Use while loop 
+"""
+# Initialize the counter
+counter = 50
+
+# Use a while loop to count from 50 to 100
+while counter <= 100:
+    print(counter)
+    counter += 1
+# The loop will exit when the counter exceeds 100
